f1fba125876cb7d0d2cb3f1d7a3b698b041ea442
Jelte van Boheemen <j.vanboheemen@uu.nl>
2021-11-24 18:02:00 +0100

    Fix showtns import

--- asta_queries.py
+++ asta_queries.py
@@ -1,12 +1,12 @@
 from config import SDLOGGER
-from dedup import getposition, showtns
+from dedup import getposition
 from macros import expandmacros
 from metadata import (longrep, repeated, repeatedseqtoken, repetition,
                       substringrep)
 from stringfunctions import string2list
 from treebankfunctions import (asta_recognised_lexnode,
                                asta_recognised_nounnode, clausecats, find1,
-                               getattval, getnodeyield, getyield)
+                               getattval, getnodeyield, getyield, showtns)
 
 noun_xpath = './/node[%asta_noun%]'
 expanded_noun_xpath = expandmacros(noun_xpath)
