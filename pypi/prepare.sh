2a5210059218d41a1e507aed3795bc81c2ac5f1d
Sheean Spoel <s.j.j.spoel@uu.nl>
2022-05-18 15:35:38 +0200

    Confusion importing config.py from editable module

--- pypi/prepare.sh
+++ pypi/prepare.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 find sastadev/ -type f -not -name '.gitignore' -delete
 TARGET=$PWD/../pypi/sastadev/
-cp config.py $TARGET
+cp __config__.py $TARGET/config.py
 cd ..
 cp LICENSE __init__.py deregularise.py inflectioncorrection.tsv.txt $TARGET
 
